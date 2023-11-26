2ca3011197b3be731fdeec5b2d5b5e15e2ca2925
ceofast <cembikmaz.ie@gmail.com>
2023-11-26 20:18:43 +0300

    tensorler ile ilgili işlemler

--- main.py
+++ main.py
@@ -1,37 +1,46 @@
+# Bu kısım, PyTorch kütüphanesini içe katarıp eğer mevcutsa GPU'yu değilse CPU'yu işlem yapmak için kullanır.
 import torch
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(device)
+
+#######################################################################################################
 block_size = 8
 batch_size = 4
 
 with open(r'C:\Users\cembi\PycharmProjects\heLaLceM\dataset\wizard_of_oz.txt', 'r', encoding='utf-8') as f:
     text = f.read()
-
+# Burada ilk 200 karakteri ekrana yazdırdık.
 print(text[:200])
+#######################################################################################################
 chars = sorted(set(text))
 print(chars)
 print(len(chars))
 vocabulary_size = len(chars)
-
 string_to_int = {ch:i for i, ch in enumerate(chars) }
 int_to_string = { i:ch for i, ch in enumerate(chars) }
 encode = lambda s: [string_to_int[c] for c in s]
 decode = lambda l: ''.join([int_to_string[i] for i in l])
-
+# Burada metindeki benzersiz karakterlerin bir seti oluşturulur ve bunlar sayısal değerlerle kodlanır.
+# Bu, metin tabanlı verileri makine öğrenmesi modelleri için işlenebilir hale getirmek için yapılır.
+#######################################################################################################
+# Burada hello kelimesi kodlanır ve decodlanır. Bu kodlama ve dekodlama işlevlerin doğruluğunu
+# test etmek içindir.
 print(encode('hello'))
-
 encoded_hello = encode('hello')
 decoded_hello = decode(encoded_hello)
 print(decoded_hello)
-
+#######################################################################################################
+# Tüm metin, sayısal değerlerle kodlanır ve bir PyTorch tensör'üne dönüştürülür. Daha sonra veri,
+# eğitim ve doğrulama setleri olarak bölünür.
 data = torch.tensor(encode(text), dtype = torch.long)
 print(data[:100])
 
 n = int(0.8*len(data))
 train_data = data[:n]
 val_data = data[n:]
-
-
+#######################################################################################################
+# Bu kısım, eğitim verileri üzerinde bir döngü oluşturur ve her adımda, belirli bir metin paröasının
+# (context) sonraki karakterini (target) tahmin etmek için kullanılacağını gösterir.
 x = train_data[:block_size]
 y = train_data[1:block_size+1]
 
@@ -39,3 +48,14 @@ for t in range(block_size):
     context = x[:t+1]
     target = y[t]
     print('When input is', context, 'target is', target)
+# Genel olarak, bu kod, metin tabanlı verileri işlemek ve makine öğrenimi modeli eğitmek için hazırlamak
+# amacıyla yazılmıştır. Özellikle, karakter düzeyinde dil modelleme veya benzeri bir görev için kullanılacaktır.
+# When input is tensor([91]) target is tensor(48)
+# When input is tensor([91, 48]) target is tensor(65)
+# When input is tensor([91, 48, 65]) target is tensor(62)
+# When input is tensor([91, 48, 65, 62]) target is tensor(1)
+# When input is tensor([91, 48, 65, 62,  1]) target is tensor(44)
+# When input is tensor([91, 48, 65, 62,  1, 44]) target is tensor(75)
+# When input is tensor([91, 48, 65, 62,  1, 44, 75]) target is tensor(72)
+# When input is tensor([91, 48, 65, 62,  1, 44, 75, 72]) target is tensor(67)
+
--- main_2.py
+++ main_2.py
@@ -1,23 +1,91 @@
 import torch
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(device)
 
 randint = torch.randint(-100, 100, (6,))
 randint
-
+# Burada, 6 elemanlı, -100 ile 100 arasında rastgele tam sayılar içeren bir tensor oluşturulur.
+############################################################################################################
 tensor = torch.tensor([[0.1, 1.2], [2.2, 3.1], [4.9, 5.2]])
 tensor
-
 zeros = torch.zeros(2, 3)
 zeros
-
 ones = torch.ones(3, 4)
 ones
-
 input = torch.empty(2, 3)
 input
-
 arange = torch.arange(5)
 arange
-
 linspace = torch.linspace(3, 10, steps = 5)
 linspace
+logspace = torch.logspace(start=-10, end=10, steps=5)
+logspace
+eye = torch.eye(5)
+eye
+a = torch.empty((2,3), dtype=torch.int64)
+empty_like = torch.empty_like(a)
+empty_like
+# Bu kısımda çeşitli tensorler oluşturuluyor: Belirli değerlere, sıfırlarla, birlerle, boş, aralıklı
+# değerlerle, logaritmik aralıklı değerlerle dolu tensorler ve birim matris.
+###############################################################################################################
+# Zaman Ölçümü ve Büyük Tensör İşlemleri
+import torch
+import numpy as np
+import time
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(device)
+
+%%time
+start_time = time.time()
+# matrix operation here
+zeros = torch.zeros(1, 1)
+end_time = time.time()
+
+elapsed_time = end_time - start_time
+print(f"{elapsed_time:.8f}")
+# Using thr time library to record execution time %%time to record the time taken for the cell to execute
+# Burada, bir tensor oluşturma işleminin ne kadar sürede tanımlandığını ölçmek için 'time' kütüphanesi
+# kullanılıyor.
+###############################################################################################################
+torch_rand1 = torch.rand(100, 100, 100, 100).to(device)
+torch_rand2 = torch.rand(100, 100, 100, 100).to(device)
+np_rand1 = torch.rand(100, 100, 100, 100)
+np_rand2 = torch.rand(100, 100, 100, 100)
+
+start_time = time.time()
+
+rand = (torch_rand1 @ torch_rand2)
+print(f"{elapsed_time:.8f}")
+
+
+start_time = time.time()
+
+rand = np.multiply(np_rand1, np_rand2)
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"{elapsed_time:.8f}")
+# Bu bölümde, büyük boyutlu tensorler üzerinde işlem yapılıyor ve bu işlemlerin süreleri ölçülüyor.
+# 'torch_rand1 @ torch_rand2' ifadesi, iki büyük tensorun matris çarpımını gerçekleştirir.
+#############################################################################################################
+# torch.stack, torch.multinominal, torch.tril, torch.triu, input.T / input.transpose, nn.Linear, torch,cat,
+# F.softmax (show all the
+
+# Define a probability tensor
+probabilities = torch.tensor([0.1, 0.9])
+# 10% or 0.1 => 0, %90 => 1. each probability points to the index of the probability in the tensor
+# Draw 5 samples from multinominal distribution
+samples = torch.multinomial(probabilities, num_samples=10, replacement=True)
+print(samples)
+tensor = torch.tensor([1, 2, 3, 4])
+out = torch.cat((tensor, torch.tensor([5])), dim = 0)
+out
+out = torch.tril(torch.ones(5, 5))
+out
+out = torch.triu(torch.ones(5, 5))
+out
+out = torch.zeros(5, 5).masked_fill(torch.tril(torch.ones(5, 5)) == 0, float('-inf'))
+out
+# Bu kısımda daha gelişmiş tensor işlemleri gerçekleştiriliyor: multinominal dağılımdan örnekler çekme,
+# tensorleri birleştirme, alt üçgen ve üst üçgen matrisler oluşturma ve maskeleme işlemleri yapılıyor.
+
 
